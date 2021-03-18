b914b07e6eafcf3731a162d78a1f38b32b0b25cf
Delph <delph_teb@hotmail.com>
2021-03-18 00:53:06 -0400

    add basic components for nav and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer p-3 mb-2 bg-dark text-white">
+      <i>
+        &#169; 2021 Copyright 
+        <strong> 
+          <a href="https://github.com/Delph-Sunny"> DT</a>
+        </strong>
+      </i>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return (
+      <nav className="jumbotron bg-dark text-white">    
+          <div className="container">
+            <h1 className="display-4">
+                Employee Directory
+            </h1>
+          </div>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
