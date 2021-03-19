e8e98573b62de3ec43e88c5411b4c05061aff949
Delph <delph_teb@hotmail.com>
2021-03-19 02:59:55 -0400

    fix structure and API call

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=100&seed=foobar&exc=login,registered";
+const BASEURL = "https://randomuser.me/api/?results=100&nat=fr&seed=foobar&exc=login,registered";
 
 
 export default {
-  search: function() {
+  getEmployeeList: function() {
     return axios.get(BASEURL);
   }
 };
